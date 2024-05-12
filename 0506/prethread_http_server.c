b48261025c3c7b9ed86f9f7e9bb6cc6bd6d2a8c0
Ha Nguyen <captainnemot1k60@gmail.com>
2024-05-12 18:32:10 +0700

    refactor: Update prethread_http_server.c to handle multiple client connections concurrently

--- 0506/prethread_http_server.c
+++ 0506/prethread_http_server.c
@@ -39,41 +39,32 @@ int main()
     int num_threads = 8;
     pthread_t thread_id;
     for (int i = 0; i < num_threads; i++)
-    {
-        int ret = pthread_create(&thread_id, NULL, thread_proc, &listener);
-        if (ret != 0)
-            printf("Could not create new thread.\n");
-        sched_yield();
-    }
+        pthread_create(&thread_id, NULL, thread_proc, &listener);
+
     pthread_join(thread_id, NULL);
 
     close(listener);
-
     return 0;
 }
 
-void *thread_proc(void *arg)
+void *thread_proc(void *param)
 {
-    int listener = *(int *)arg;
+    int listener = *(int *)param;
     char buf[256];
     while (1)
     {
-        // Chờ kết nối
         int client = accept(listener, NULL, NULL);
-        printf("New client %d accepted in thread %ld with pid %d\n", client,
-               pthread_self(), getpid());
-        // Chờ dữ liệu từ client
+        printf("new client accepted: %d\n", client);
         int ret = recv(client, buf, sizeof(buf), 0);
         if (ret <= 0)
             continue;
-        // Xử lý dữ liệu, trả lại kết quả cho client
-        buf[ret] = 0;
-        printf("Received from client %d: %s\n", client, buf);
 
-        char *message = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello world!</h1></body></html>";
-        send(client, message, strlen(message), 0);
-        // Đóng kết nối
+        buf[ret] = 0;
+        printf("Received: %s\n", buf);
+
+        strcpy(buf, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello World</h1></body></html>");
+        send(client, buf, strlen(buf), 0);
+
         close(client);
     }
-    return NULL;
 }
