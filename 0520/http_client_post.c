7d69da78a6093beeb4f0c2f1d2b75a9653361ced
Ha Nguyen <captainnemot1k60@gmail.com>
2024-05-26 22:33:28 +0700

    feat: btvn2005

---
+++ 0520/http_client_post.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+
+int main()
+{
+    int client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    // Phan giai ten mien http://httpbin.org
+    struct addrinfo *info;
+    if (getaddrinfo("httpbin.org", "http", NULL, &info) != 0)
+    {
+        perror("getaddrinfo() failed");
+        return 1;
+    }
+
+    int ret = connect(client, info->ai_addr, info->ai_addrlen);
+    if (ret == -1)
+    {
+        perror("connect() failed");
+        return 1;
+    }
+
+    char req[] = "POST /post HTTP/1.1\r\nHost: httpbin.org\r\nContent-length: 27\r\nparam1=value1&param2=value2";
+
+    send(client, req, strlen(req), 0);
+
+    char buf[2048];
+    while (1)
+    {
+        ret = recv(client, buf, sizeof(buf), 0);
+        if (ret <= 0)
+        {
+            close(client);
+            break;
+        }
+        buf[ret] = 0;
+        printf("%d bytes received\n%s\n", ret, buf);
+    }
+
+    close(client);
+}
